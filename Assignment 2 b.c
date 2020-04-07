aea613615c1585a09fda17377e6ee99fb09ecfe6
gayathrisuresh14 <63206998+gayathrisuresh14@users.noreply.github.com>
2020-04-07 15:04:56 +0530

    Add files via upload

---
+++ Assignment 2 b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  
+  printf("%s is my pet\n","Mickey ");
+  printf("Now she is %d years old\n",2);
+  printf("%s loves to eat %s\n","Mickey","fish");
+  return 0;
+}
+
