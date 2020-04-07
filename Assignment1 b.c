ac29730266dcf4a304b11d36c3178bf1f9897af6
gayathrisuresh14 <63206998+gayathrisuresh14@users.noreply.github.com>
2020-04-07 14:58:34 +0530

    Add files via upload

---
+++ Assignment1 b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  printf("Gayathri M S\n");
+  printf("Electronics and Communication Engineering\n");
+  printf("College of Engineering Chengannur\n");
+  return 0;
+}
+
