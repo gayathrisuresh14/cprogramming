a3d9131beac5d1fa941484c2fec2cc5ad210afd9
gayathrisuresh14 <63206998+gayathrisuresh14@users.noreply.github.com>
2020-04-08 18:22:24 +0530

    Add files via upload

---
+++ Assignment 6b .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int n;
+  printf("Enter a number:");
+  scanf("%d", &n);
+  do{
+    printf("%d\n",n);
+    n--;
+  } while (n>=0);
+  return 0;
+}
