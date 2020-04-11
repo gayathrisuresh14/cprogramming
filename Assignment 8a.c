de2a4393fd7251baaa76702fc9c688010db6e5d8
gayathrisuresh14 <63206998+gayathrisuresh14@users.noreply.github.com>
2020-04-11 21:22:46 +0530

    Add files via upload

---
+++ Assignment 8a.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void main()
+{
+  char str[10];
+  int i;
+  printf("\nEnter a string");
+  gets(str);
+  for(i=0;str[i]!='\0';i++)
+  {
+    if(str[i]>=65 && str[i]<=90)
+   (str[i]=str[i]+32);
+   else 
+   (str[i]=str[i]-32);
+  }
+  printf("\nConverted String:%s",str);
+  return 0;
+}
